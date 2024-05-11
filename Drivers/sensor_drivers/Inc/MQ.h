4e6decf70d810ce5cf9388d835294cd2d867a229
MRTURK0X1 <144950208+MRTURK0X1@users.noreply.github.com>
2024-05-11 23:16:48 +0300

    Add files via upload

---
+++ Drivers/sensor_drivers/Inc/MQ.h
@@ -0,0 +1,74 @@
+#ifndef MQ_H
+#define MQ_H
+
+#include "stm32f3xx_hal.h"
+#include <math.h>
+/// Resistor on Sensor in kΩ
+#define RL 80
+
+/// Voltage on Sensor in V
+#define VIn 5
+
+/// Board analog Input Resolution
+/// Default: 2^12
+//#define Resolution 4095
+
+/// CO2 Level in Atmosphere
+#define ATMOCO2 397.13
+
+//Correction Values
+#define CorrA -0.000002469136
+#define CorrB 0.00048148148
+#define CorrC 0.0274074074
+#define CorrD 1.37530864197
+#define CorrE 0.0019230769
+
+#define Vstep (float)VIn/(4095-1)
+
+
+
+typedef struct _MQ{
+
+	ADC_HandleTypeDef* ADC;
+	float R0;
+	uint8_t temp;
+	uint8_t hum;
+
+}MQ;
+
+void MQ_Init(MQ*mq,ADC_HandleTypeDef*adc,uint8_t temp,uint8_t hum);
+
+void update_tempHum(MQ*mq135,uint8_t temp,uint8_t hum);
+
+/// Assume CO2 Level is the default Atmospheric Level (~400ppm)
+float getR0(MQ*mq);
+float getCorrectedR0(MQ*mq);
+
+/// Gets the resolved sensor voltage
+float getVoltage(ADC_HandleTypeDef* hadc);
+
+/// Calculate Correction Factor depending on temparature and humidity
+float getCorrectionFactor(MQ*mq);
+
+/// Calculates the Resistance of the Sensor
+float getResistance(MQ*mq);
+float getCorrectedResistance(MQ*mq);
+/// Calculates ppm on a exponential curve
+/// (Different Gases have different curves)
+float getPPM(MQ*mq, float m, float b);
+float getPPM_a(MQ*mq,float m,float b);
+float getCorrectedPPM(MQ*mq, float m, float b);
+
+/// Gets ppm of ammonia in Air (NH3)
+float getNH3(MQ*mq);
+float getCorrectedNH3(MQ*mq);
+float getNH3_a(MQ*mq);
+/// Gets ppm of CO2 in Air
+float getCO2(MQ*mq);
+float getCorrectedCO2(MQ*mq);
+
+float getCH4(MQ*mq);
+
+
+#endif
+
---
+++ Drivers/sensor_drivers/Src/MQ.c
@@ -0,0 +1,112 @@
+#include <MQ.h>
+
+void MQ_Init(MQ*mq,ADC_HandleTypeDef*adc,uint8_t temp,uint8_t hum){
+
+	mq->temp = temp;
+	mq->hum = hum;
+	mq->ADC = adc;
+	mq->R0 = getR0(mq);
+
+}
+
+void update_tempHum(MQ*mq135,uint8_t temp,uint8_t hum){
+	mq135->temp = temp;
+	mq135->hum = hum;
+}
+
+float getVoltage(ADC_HandleTypeDef* hadc) { //an average function
+
+	HAL_ADC_Start(hadc);
+	HAL_ADC_PollForConversion(hadc, 100);
+	uint16_t adcRead = HAL_ADC_GetValue(hadc);
+	HAL_ADC_Stop(hadc);
+
+	return  (float)adcRead * Vstep; // PRODUCT With v step
+
+}
+
+float getCorrectionFactor(MQ*mq135) {
+    return (CorrA * pow(mq135->temp, 3) + CorrB * pow(mq135->temp, 2) - CorrC * mq135->temp + CorrD - (CorrE * mq135->hum - CorrE * 33));
+}
+
+float getResistance(MQ*mq135) {
+    float voltage = getVoltage(mq135->ADC);
+    float rs = ((5 * RL) / voltage) - (float)RL;
+    if (rs < 0) {
+        rs = 0;
+    }
+    return rs;
+}
+
+float getCorrectedResistance(MQ*mq135) {
+    return getResistance(mq135) / getCorrectionFactor(mq135);
+}
+
+
+float getR0(MQ*mq135) {
+
+	float sum;
+
+	for(uint8_t i = 0;i<50;i++){
+    sum = sum + getResistance(mq135);
+    HAL_Delay(100);
+	}
+	float r0  = sum/50;
+    return r0;
+}
+
+float getCorrectedR0(MQ*mq135) {
+    float r0 = getCorrectedResistance(mq135);
+    return r0;
+}
+
+
+float getPPM(MQ*mq,float m,float b) {
+    float ratio = getResistance(mq) / mq->R0;
+    float ppm_log = (log10(ratio) - b) / m;
+    float ppm = pow(10, ppm_log);
+    if (ppm < 0) {
+        ppm = 0;
+    }
+    return ppm;
+}
+
+float getPPM_a(MQ*mq,float m,float b) {
+    float ratio = getResistance(mq)*3.6 / (mq->R0);
+    float ppm_log = (log10(ratio) - b) / m;
+    float ppm = pow(10, ppm_log);
+    if (ppm < 0) {
+        ppm = 0;
+    }
+    return ppm;
+}
+
+float getCorrectedPPM(MQ*mq,float m,float b) {
+    float ratio = getCorrectedResistance(mq) / mq->R0;
+    float ppm_log = (log10(ratio) - b) / m;
+    float ppm = pow(10, ppm_log);
+    if (ppm < 0) {
+        ppm = 0;
+    }
+    return ppm;
+}
+
+
+float getCorrectedCO2(MQ*mq) {
+    return getCorrectedPPM(mq,110.47, -2.862) + ATMOCO2;
+}
+
+float getCO2(MQ*mq) {
+    return getPPM(mq,110.47, -2.862) + ATMOCO2;
+}
+
+float getNH3(MQ*mq) {
+    return getPPM(mq,-0.2640,-0.2307);
+}
+
+float getNH3_a(MQ*mq) {
+    return getPPM_a(mq,-0.2784,0.4460);
+}
+float getCH4(MQ*mq) {
+    return getPPM_a(mq,-0.600553,0.801659);
+}
